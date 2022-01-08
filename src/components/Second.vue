236ed7d10fdc871ca36c9befc7d0deea9e7f9a87
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-08 19:04:22 +0800

    Added second component

---
+++ src/components/Second.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <h1 v-scrollanimation>Second</h1>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "Second",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
