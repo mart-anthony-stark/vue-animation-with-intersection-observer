b4ae888f09c0d1be6c598d69f76cf0512209459c
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-08 19:04:31 +0800

    Added custom directive

---
+++ src/directives/scrollAnimation.js
@@ -0,0 +1,15 @@
+const animatedScrollObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("enter");
+      animatedScrollObserver.unobserve(entry.target);
+    }
+  });
+});
+
+export default {
+  bind(el) {
+    el.classList.add("before-enter");
+    animatedScrollObserver.observe(el);
+  },
+};
